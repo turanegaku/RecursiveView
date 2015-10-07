737d8650c7b5a8302bcaa51490fa6aaa1cc9b2e7
turanegaku <turanegaku@gmail.com>
2015-10-07 13:32:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+RecursiveView
+====
+
+## Overview
+This is Recursive Image Viewer using JavaFX.
+
+## Description
+- This application show you **ALL** Image file in the directory.
+- You know what images in directories which is in the specified directory.
+- You can see the images up down scroll.
+- Click ``Next`` or ``Prev`` button, then viewer show the next Directory
+
+## Requirement
+- JavaFX
+- Java8
+
+## Example
+This is sample Directory.
+```
+specified_directory/
++-- animal/
+	+-- bird/
+		+-- bird1.png
+		+-- bird2.png
+		+-- bird3.png
+		+-- bird4.png
+		+-- bird5.png
+		+-- bird6.png
+	+-- cat.png
+	+-- dog.png
++-- food/
+	+-- egg.png
+	+-- cake.png
+	+-- rice.png
++-- charactor/
+	+-- a.png
+	+-- b.png
+	+-- c.png
+	+-- d.png
+	+-- e.png
+	+-- f.png
+```
+
+You can first see bird1~5.png, cat.png, dog.png.  
+``Next``  
+egg.png, cake.png, rice.png  
+``Next``  
+a ~ f.png
---
+++ src/turane/app/MainApp.java
@@ -0,0 +1,112 @@
+package turane.app;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ListIterator;
+import java.util.stream.Collectors;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	@FXML
+	private ListView<ImageView> imgs;
+	@FXML
+	private TextField folder;
+	@FXML
+	private TextField directory;
+	@FXML
+	private Button prev, next;
+
+	private 
+	ListIterator<Path> files = null;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Pane root = FXMLLoader.load(getClass().getResource("main.fxml"));
+		stage.setScene(new Scene(root));
+
+		stage.show();
+	}
+
+	@FXML
+	public void dir(ActionEvent e){
+		try {
+			files = Files.list(Paths.get(directory.getText()))
+					.filter(p->Files.isDirectory(p))
+					.collect(Collectors.toList())
+					.listIterator();
+			next.requestFocus();
+			nxt(e);
+		} catch (IOException e1) {
+			files = null;
+			e1.printStackTrace();
+		}
+	}
+
+	private void setImages(Path f){
+		folder.setText(f.getFileName().toString());
+		try {
+			Files.find(f, Integer.MAX_VALUE, (path, attr) -> path.getFileName().toString().matches(".*\\.(jpg)*(png)*"))
+			.filter(path->!Files.isDirectory(path))
+			.forEach(path->{
+				try {
+					InputStream is = Files.newInputStream(path, StandardOpenOption.READ);
+					ImageView iv = new ImageView(new Image(is));
+					iv.fitWidthProperty().bind(imgs.widthProperty().subtract(20));
+					iv.setPreserveRatio(true);
+					imgs.getItems().add(iv);
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	private void prv(ActionEvent e){
+		imgs.getItems().clear();
+		if (files == null) {
+			folder.setText("Fail to load root Directory");
+			return;
+		} else if (!files.hasPrevious()){
+			folder.setText("Reach to final items");
+			return;
+		}
+		setImages(files.previous());
+	}
+	@FXML
+	private void nxt(ActionEvent e){
+		imgs.getItems().clear();
+		if (files == null) {
+			folder.setText("Fail to load root Directory");
+			return;
+		} else if (!files.hasNext()){
+			folder.setText("Reach to final items");
+			return;
+		}
+		setImages(files.next());
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
